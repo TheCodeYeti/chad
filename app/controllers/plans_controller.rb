bc4127a0b044c7fd6892f944a717d5dfb73ecd83
funzone36 <funzone36@gmail.com>
2016-08-20 16:10:58 -0400

    created the plan controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,15 @@
 class PlansController < ApplicationController
   def edit
+    if @plans
+      @plans = Plan.all
+    else
+      @plans = Plan.new
+    end
   end
 
   def update
+
   end
+
+
 end
